20bae297fb0a3aed7a8351bc8db17c7be25a8449
dudelim <137728498+dudelim@users.noreply.github.com>
2023-07-06 16:26:30 +0000

    1

--- .ccls-cache/@home@runner@removeduplicata/main.c
+++ .ccls-cache/@home@runner@removeduplicata/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void) {
-  printf("Hello World\n");
-  return 0;
+   char palavra[50];
+  int i, j, k;
+
+  printf("Input: ");
+  fgets(palavra, 50, stdin);
+
+  for (i = 0; i < strlen(palavra); i++) {
+    for (j = (1 + i); palavra[j] != '\0'; j++) {
+      if (palavra[i] == palavra[j]) {
+    
 }
